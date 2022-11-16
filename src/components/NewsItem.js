2c6382940afdc038032a1bcf72d2cc72970ba5ab
Roshan <its.roshanojha@gmail.com>
2022-11-16 13:02:41 +0530

    Getting started with news app

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is a news Item 4
+      </div>
+    )
+  }
+}
+
+export default NewsItem
